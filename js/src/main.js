bee696f6c6d29a531234017229f51aa0cf9fa2ad
Pasondese <53469937+Pasondese@users.noreply.github.com>
2024-06-25 14:47:40 +0300

    Initial Commit

---
+++ js/src/main.js
@@ -0,0 +1,31 @@
+if (document.querySelector(".header")) {
+    const   headerBurger = document.querySelector(".header__burger"),
+            headerNav = document.querySelector(".header__nav"),
+            headerNavItems = document.querySelectorAll(".header__nav *");
+
+    const mobileNavOpen = () => {
+        document.body.classList.add("body--not-scroll");
+        headerNav.classList.add("nav--active");
+        headerBurger.classList.add("header__burger--active");
+    }
+
+    const mobileNavClose = () => {
+        document.body.classList.remove("body--not-scroll");
+        headerNav.classList.remove("nav--active");
+        headerBurger.classList.remove("header__burger--active");
+    }
+
+    headerBurger.addEventListener("click", () => {
+        if (!headerNav.classList.contains("nav--active")) {
+            mobileNavOpen();
+        } else {
+            mobileNavClose();
+        }
+    });
+
+    headerNavItems.forEach((headerNavItems_item) => {
+        headerNavItems_item.addEventListener("click", () => {
+            mobileNavClose();
+        });
+    });
+}
